0bc2a4692f72612a576423db51cbea4bffce7611
Valentin <valentin.lucet@gmail.com>
2021-03-15 19:43:36 -0400

    nake model 4_1

--- 1_models.jl
+++ 1_models.jl
@@ -1,20 +1,29 @@
-# m4.1
+## STATS RETHINKING PRACTICE
 
+# Needed libraries
 import CSV
-
 using DataFrames
 using Turing
 using TuringModels
+using StatsPlots
+using StatsBase
+using Optim
 
-data_path = joinpath(TuringModels.project_root,"StatisticalRethinking.jl", "data", "Howell1.csv")
+# Data
+data_path = joinpath(TuringModels.project_root, "data", "Howell1.csv")
 df = CSV.read(data_path, DataFrame; delim=';')
 df = filter(row -> row.age >= 18, df)
 
-@model function line(height)
+# m4_1
+@model function m4_1(height)
     σ ~ Uniform(0, 50)
     μ ~ Normal(178, 20)
 
     height .~ Normal.(μ, σ)
 end
 
-model = line(df.height)
+m4_1_model = m4_1(df.height)
+m4_1_chains = sample(m4_1_model, NUTS(0.65), 1000)
+StatsPlots.plot(m4_1_chains)
+m4_1_map_estimate = optimize(m4_1_model, MAP())
+vcov(m4_1_map_estimate)
