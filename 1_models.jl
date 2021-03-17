d127c7b23f4af0acdd2c0de8896401694b2257e4
Valentin <valentin.lucet@gmail.com>
2021-03-17 11:05:23 -0400

    a plot!

--- 1_models.jl
+++ 1_models.jl
@@ -52,14 +52,21 @@ vcov(m4_2_map_estimate)
 # end
 
 # Equivalent to:
-@model function m4_3(height, weight)
+@model function m4_3(height, weight, weight_mean)
+    
+    if ismissing(weight_mean)
+        weight_mean = mean(weight)
+    end
+    
     α ~ Normal(178, 20)
     β ~ LogNormal(0, 1)
-    μ = α .+ β .* (weight.-mean(weight))
+    μ = α .+ β .* (weight.-weight_mean)
     σ ~ LogNormal(0, 5) # Changes the prior here
+
     for i in 1:length(height)
         height[i] ~ Normal(μ[i], σ)
     end
+
 end
 
 m4_3_model = m4_3(df.height, df.weight)
@@ -74,7 +81,6 @@ p = scatter(df.weight, df.height)
 
 for row in 1:length(m4_3_chains)
     yi = m4_3_chains[:α][row] .+ m4_3_chains[:β][row] .* (xi .- mean(df.weight))
-    # println(yi)
     plot!(p, xi, yi, alpha=0.01, color="#000000", lab="")
 end
 
@@ -97,8 +103,8 @@ quantiles = [quantile(v, [0.1, 0.9]) for v in arr]
 lower = [q[1] - m for (q, m) in zip(quantiles, m)]
 upper = [q[2] - m for (q, m) in zip(quantiles, m)]
 
-p2 = scatter(df.weight, df.height)
-plot!(p2, xi, m, ribbon = [lower, upper])
+p2 = scatter(df.weight, df.height, lab="")
+plot!(p2, xi, m, ribbon = [lower, upper], lab="")
 
 # Prediction interval
 
@@ -113,19 +119,17 @@ plot!(p2, xi, m, ribbon = [lower, upper])
 # m4_3_chains_test = sample(m4_3_model_test, NUTS(0.65), 1000)
 
 x_pred = xi
-m_test = m4_3(Vector{Union{Missing, Float64}}(undef, length(x_pred)), hcat(x_pred));
+m_test = m4_3(Vector{Union{Missing, Float64}}(undef, length(x_pred)), hcat(x_pred), mean(df.weight));
 predictions = predict(m_test, m4_3_chains)
 
 pred_array = Array(group(predictions, :height))
-for col in eachcol(pred_array)
-    print(quantile(col))
-end
-
 quantiles_pred = [quantile(col, [0.1, 0.9]) for col in eachcol(pred_array)]
 m_pred = [mean(v) for v in eachcol(pred_array)]
 lower_pred = [q[1] - m for (q, m) in zip(quantiles_pred, m_pred)]
 upper_pred = [q[2] - m for (q, m) in zip(quantiles_pred, m_pred)]
 
-p3 = scatter(df.weight, df.height)
-plot!(p3, xi, m, ribbon = [lower, upper])
-plot!(p3, x_pred, m_pred, ribbon = [lower_pred, upper_pred])
+p3 = scatter(df.weight, df.height, lab="")
+plot!(p3, xi, m, ribbon = [lower, upper], lab="")
+plot!(p3, x_pred, m_pred, ribbon = [lower_pred, upper_pred], lab="")
+
+png(p3, "plots/reg_1.png")
