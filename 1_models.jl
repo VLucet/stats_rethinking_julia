ead4dbbef9a2f77a86f1147fff17a2f26610a2c8
Valentin <valentin.lucet@gmail.com>
2021-03-18 18:39:39 -0400

    add PPC

--- 1_models.jl
+++ 1_models.jl
@@ -18,13 +18,12 @@ df = filter(row -> row.age >= 18, df)
 @model function m4_1(height)
     σ ~ Uniform(0, 50)
     μ ~ Normal(178, 20)
-
     height .~ Normal.(μ, σ)
 end
 
 m4_1_model = m4_1(df.height)
 m4_1_chains = sample(m4_1_model, NUTS(0.65), 1000)
-StatsPlots.plot(m4_1_chains)
+plot(m4_1_chains)
 m4_1_map_estimate = optimize(m4_1_model, MAP())
 vcov(m4_1_map_estimate)
 
@@ -32,7 +31,6 @@ vcov(m4_1_map_estimate)
 @model function m4_2(height)  
     σ ~ Uniform(0, 50)
     μ ~ Normal(178, 0.1)
-
     height .~ Normal.(μ, σ)
 end
 
@@ -51,7 +49,7 @@ vcov(m4_2_map_estimate)
 #     height .~ Normal.(μ, σ)
 # end
 
-# Equivalent to:
+# Equivalent to what folows, but smoehow loop is better for predictions
 @model function m4_3(height, weight, weight_mean)
     
     if ismissing(weight_mean)
@@ -69,7 +67,54 @@ vcov(m4_2_map_estimate)
 
 end
 
+# Different prior on β
+@model function m4_3_2(height, weight, weight_mean)
+    
+    if ismissing(weight_mean)
+        weight_mean = mean(weight)
+    end
+    
+    α ~ Normal(178, 20)
+    β ~ LogNormal(0, 10)
+    μ = α .+ β .* (weight.-weight_mean)
+    σ ~ LogNormal(0, 5) # Changes the prior here
+
+    for i in 1:length(height)
+        height[i] ~ Normal(μ[i], σ)
+    end
+
+end
+
 m4_3_model = m4_3(df.height, df.weight, missing)
+m4_3_2_model = m4_3_2(df.height, df.weight, missing)
+
+# Prior predictive check
+
+m4_3_chains_prior = sample(m4_3_model, Prior(), 100)
+m4_3_2_chains_prior = sample(m4_3_2_model, Prior(), 100)
+
+p = plot();
+for row in 1:length(m4_3_chains_prior)
+    yi = m4_3_chains_prior[:α][row] .+ m4_3_chains_prior[:β][row] .* (xi .- mean(df.weight))
+    plot!(p, xi, yi, alpha=0.3, color="#000000", lab="")
+end
+hline!(p, [0], ls=:dash, color="#000000", lab="");
+hline!(p, [272], color="#000000", lab="");
+plot!(p, ylims = [-100, 400], lab="", title="log(b) ~ N(0, 1)");
+
+p2 = plot();
+for row in 1:length(m4_3_2_chains_prior)
+    yi = m4_3_2_chains_prior[:α][row] .+ m4_3_2_chains_prior[:β][row] .* (xi .- mean(df.weight))
+    plot!(p2, xi, yi, alpha=0.3, color="#000000", lab="")
+end
+hline!(p2, [0], ls=:dash, color="#000000", lab="");
+hline!(p2, [272], color="#000000", lab="");
+plot!(p2, ylims = [-100, 400], lab="", title="log(b) ~ N(0, 10)");
+
+plot(p, p2, layout = (1, 2), legend = false)
+
+# Sampling
+
 # m4_3_chains = sample(m4_3_model, NUTS(), MCMCThreads(), 1000, 4)
 m4_3_chains = sample(m4_3_model, NUTS(0.65), 1000)
 StatsPlots.plot(m4_3_chains)
@@ -77,7 +122,7 @@ m4_3_map_estimate = optimize(m4_3_model, MAP())
 vcov(m4_3_map_estimate)
 
 xi = minimum(df.weight):0.1:maximum(df.weight)
-p = scatter(df.weight, df.height)
+p = scatter(df.weight, df.height, lab="")
 
 for row in 1:length(m4_3_chains)
     yi = m4_3_chains[:α][row] .+ m4_3_chains[:β][row] .* (xi .- mean(df.weight))
@@ -107,16 +152,6 @@ p2 = scatter(df.weight, df.height, lab="")
 plot!(p2, xi, m, ribbon = [lower, upper], lab="")
 
 # Prediction interval
-
-# @model function m4_3_test(height, weight)
-#     α ~ Normal(178, 20)
-#     β ~ LogNormal(0, 1)
-#     μ = α .+ β .* (weight.-mean(weight))
-#     σ = 0.1
-#     height .~ Normal.(μ, σ)
-# end
-# m4_3_model_test = m4_3_test(df.height, df.weight)
-# m4_3_chains_test = sample(m4_3_model_test, NUTS(0.65), 1000)
 
 x_pred = xi
 m_test = m4_3(Vector{Union{Missing, Float64}}(undef, length(x_pred)), hcat(x_pred), mean(df.weight));
