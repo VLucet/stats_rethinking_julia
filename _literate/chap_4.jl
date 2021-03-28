b536193c85188bbd411a061b17fad32e1a159dbb
Valentin <valentin.lucet@gmail.com>
2021-03-27 23:51:43 -0400

    add figure 4.4

--- _literate/chap_4.jl
+++ _literate/chap_4.jl
@@ -92,7 +92,38 @@ savefig(figure_4_3, joinpath(@OUTPUT, "figure_4_3.svg")); #src
 
 μ_vec = range(150, 160, length = 100)
 σ_vec = range(7, 9, length = 100)
+
 post_grid = reduce(vcat, collect(Iterators.product(μ_vec, σ_vec)))
+post_grid_dist = [Normal(t[1], t[2]) for t in post_grid]
+log_likelihood = sum.([log.(pdf.(d, howell.height)) for d in post_grid_dist])
+
+all_μ = [p[1] for p in post_grid]
+all_σ = [p[2] for p in post_grid]
+
+post_prod = log_likelihood .+ log.(pdf(Normal(178, 20), all_μ)) 
+                           .+ log.(pdf(Uniform(0, 50), all_σ))
+post_prob = exp.(post_prod .- maximum(post_prod));
+
+# We can make a contour plot of the posterior: 
+
+contour_plot = contour(μ_vec, σ_vec, post_prob)
+savefig(contour_plot, joinpath(@OUTPUT, "contour_plot.svg")); #src 
+
+# \figalt{}{contour_plot.svg}
+
+# Let's now sample from that posterior
+
+sample_rows = sample(collect(1:size(post_grid)[1]), Weights(post_prob), 
+                     10_000, replace = true)
+sample_μ = all_μ[sample_rows]
+sample_σ = all_σ[sample_rows]
+
+figure_4_4 = scatter(sample_μ, sample_σ, lab = "", aspect_ratio = 1.5,
+                     xlab = "μ", ylab = "σ", markeralpha=0.1, markersize=3);
+
+savefig(figure_4_4, joinpath(@OUTPUT, "figure_4_4.svg")); #src 
+
+# \figalt{}{figure_4_4.svg}
 
 # ## Figure 4.5
 
