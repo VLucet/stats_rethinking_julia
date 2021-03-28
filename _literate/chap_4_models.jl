da939b2b5da7ac7a15980b738a753ac86b658c8d
Valentin <valentin.lucet@gmail.com>
2021-03-28 17:02:18 -0400

    fix models

--- _literate/chap_4_models.jl
+++ _literate/chap_4_models.jl
@@ -76,9 +76,11 @@ end;
         height[i] ~ Normal(μ[i], σ)
     end
 
-end
+end;
 
 # ## 4.5
+
+# Square model:
 
 @model function m4_5(height, weight, weight_squared)
 
@@ -93,4 +95,22 @@ end
         height[i] ~ Normal(μ[i], σ)
     end
 
-end
+end;
+
+# Cube model:
+
+@model function m4_5_2(height, weight, weight_squared, weight_cubed)
+
+    α ~ Normal(178, 20)
+    β1 ~ LogNormal(0, 1)
+    β2 ~ Normal(0,10)
+    β3 ~ Normal(0,10)
+    σ ~ Uniform(0, 50) 
+
+    μ = α .+ β1 .* weight .+ β2 .* weight_squared .+ β3 .* weight_cubed
+
+    for i in 1:length(height)
+        height[i] ~ Normal(μ[i], σ)
+    end
+
+end;
