226316051c8f3f8bde0cbe0717acb2e754d2ce6b
Valentin <valentin.lucet@gmail.com>
2021-03-30 20:15:09 -0400

    add figure 5.1

--- _literate/chap_5.jl
+++ _literate/chap_5.jl
@@ -15,9 +15,17 @@ first(waffle, 5)
 # This first figure is a simple plot, I did not bother running the regression
 # however. 
 
-scatter(waffle.WaffleHouses, waffle.Divorce, lab = "", markersize = 4,
-        xlab = "Waffle houses per million", ylab = "Divorce Rate")
+figure_5_1 = scatter(waffle.WaffleHouses, waffle.Divorce, lab = "", markersize = 4,
+                     xlab = "Waffle houses per million", ylab = "Divorce Rate")
+loc_array = ["ME", "OK", "AR", "AL", "SC", "GA", "NJ"]
+loc_id = findall(x -> x in loc_array, waffle.Loc)
+waffle.Loc_annot .= " "
+waffle.Loc_annot[loc_id] .= loc_array
 annot = Array(tuple.(waffle.WaffleHouses.+5, waffle.Divorce, 
-                     text.(waffle.Loc, :left, 9)))
-annotate!(annot)
+                     text.(waffle.Loc_annot, :left, 9)))
+annotate!(figure_5_1, annot);
+
+savefig(figure_5_1, joinpath(@OUTPUT, "figure_5_1.svg")); #src 
+
+# \figalt{}{figure_4_12.svg}
 
